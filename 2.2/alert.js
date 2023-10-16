d98d42ab2c004f78a01fbbc53c68c9c75dfb55c1
Wladzimir92 <xerka16@gmail.com>
2023-10-16 21:15:07 +0300

    Feat 2.4

--- 2.2/alert.js
+++ 2.2/alert.js
@@ -1,2 +1,2 @@
-
+"use strict"
 alert ('Я JavaScript!')
---
+++ 2.4/alert.js
@@ -0,0 +1,8 @@
+"use strict"
+let admin;
+let name="Джон";
+admin=name
+alert (admin);
+
+let ourPlanetName="Earth";
+let currentUserName = "Джон";
